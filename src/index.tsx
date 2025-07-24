5e0f4969843db30bb506759baef0d9129c3b2e0a
Jeff Sears <jjsears6@gmail.com>
2025-07-24 14:00:06 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { StrictMode } from "react";
 import * as ReactDOMClient from "react-dom/client";
 
-import App from "./App";
+import App from "./App.tsx";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOMClient.createRoot(rootElement);
